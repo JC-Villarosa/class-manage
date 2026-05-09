9b8060dffd59f391014e7b3fe98b6004fa756fc2
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2026-05-09 22:47:20 +0800

    Added Email

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NotifyGuardian;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LogsController extends Controller
 {
     public function log_time_in($student_id)
     {
-        dd(Student::where('custom_id', $student_id)->with('guardians')->first()->toArray());
+        // dd(Student::where('custom_id', $student_id)->with('guardians')->first()->toArray());
+        $record = Student::where('custom_id', $student_id)->with('guardians')->first();
+
+        if ($record->guardians)
+        {
+            foreach($record->guardians as $guardian)
+            {
+                Mail::to($guardian->email)->send(new NotifyGuardian($record));
+            }
+        }
     }
 }
---
+++ resources/views/mails/test.blade.php
@@ -0,0 +1,2 @@
+<h1>Time In</h1>
+<span> Student: {{ $student->first_name }} {{ $student->last_name }}</span>
